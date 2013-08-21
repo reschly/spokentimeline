8bb3298bced33e5c77e10f46244ecb32570a8d97
Michael Reschly <michael@reschly.com>
2013-08-20 18:30:36 -0700

    starting on speech-to-voice part

--- readme.txt
+++ readme.txt
@@ -1,5 +1,6 @@
 A python program to run your twitter timeline through a voice-to-text engine.  This is either a brilliant idea or a terrible idea.  I'm not sure which.
 
 Requirements: twitter ( pip install twitter )
+			  pyttsx ( pip install pyttsx )
 
 Known issue: skips any tweet that would otherwise produce an UnicdeEncodeError
--- spokentimeline.py
+++ spokentimeline.py
@@ -1,13 +1,14 @@
 from twitter import *
+import pyttsx
 import APIKEYS
 
-
+                
 ''' MYCREDS.txt has the following format:
 oauthtokenvalue
 oauthsecretvalue
 '''
 
-def getTwitter():
+def getTwitterByConfig():
     oauth_token, oauth_secret = read_token_file("MYCREDS.txt")
     twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, APIKEYS.SPOKENTIMELINE_CONSUMERKEY, APIKEYS.SPOKENTIMELINE_CONSUMERSECRET))
     return twitter
@@ -18,11 +19,27 @@ def printTimeline(timeline):
             print(tweet['user']['screen_name'] + " at " + tweet['created_at'] + " tweeted " + tweet['text']);
         except UnicodeEncodeError:
             pass;
+
+def formatTimeline(timeline):
+    result = []
+    for tweet in timeline:
+        result.append(tweet['user']['screen_name'] + " at " + tweet['created_at'] + " tweeted " + tweet['text'])
+    return result
         
 def getTimeline(twitter):
     return twitter.statuses.home_timeline()
+ 
+
+def getSpeechEngine():
+    return pyttsx.init()
+
+def speakTimeline(engine, timeline):
+    for tweet in formatTimeline(timeline):
+        engine.say(tweet)
     
 if __name__ == "__main__":
-    t = getTwitter()
+    t = getTwitterByConfig()
     timeline = getTimeline(t)
     printTimeline(timeline)
+    engine = getSpeechEngine()
+    speakTimeline(engine, timeline)
